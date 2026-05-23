b66e755e1077aa3333639cf59139b7febf4a84a6
Frank <framis@uade.edu.ar>
2026-05-23 20:20:17 -0300

    Agrego Carrera Controller, me voy a dormir

--- src/main/java/com/uade/carreracaballos/controller/CarreraController.java
+++ src/main/java/com/uade/carreracaballos/controller/CarreraController.java
@@ -1,5 +1,60 @@
 package com.uade.carreracaballos.controller;
 
+import java.util.List;
+
+import com.uade.carreracaballos.model.Caballo;
+import com.uade.carreracaballos.model.Carrera;
+import com.uade.carreracaballos.model.EstadoCarrera;
+
 public class CarreraController {
 
+    private Carrera carrera;
+
+    public CarreraController() {
+    }
+
+    public void crearCarrera(List<Caballo> caballos, int longitudPista) {
+
+        carrera = new Carrera(caballos, longitudPista);
+    }
+
+    public void iniciarCarrera() {
+
+        if (carrera != null) {
+            carrera.iniciarCarrera();
+        }
+    }
+
+    public boolean avanzarInstante() {
+
+        if (carrera == null) {
+            return false;
+        }
+
+        if (carrera.getEstado() == EstadoCarrera.FINALIZADA) {
+            return false;
+        }
+
+        carrera.avanzarCorredores();
+
+        return true;
+    }
+
+    public List<Caballo> obtenerPosiciones() {
+
+        if (carrera == null) {
+            return null;
+        }
+
+        return carrera.obtenerPosiciones();
+    }
+
+    public EstadoCarrera obtenerEstado() {
+
+        if (carrera == null) {
+            return null;
+        }
+
+        return carrera.getEstado();
+    }
 }
