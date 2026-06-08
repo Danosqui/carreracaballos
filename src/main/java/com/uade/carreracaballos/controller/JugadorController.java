3a32e0426e06702d7802370025db7574a0ee56b2
JuanValcarce <vjuan@frba.utn.edu.ar>
2026-06-08 19:17:43 -0300

    Update JugadorController.java

--- src/main/java/com/uade/carreracaballos/controller/JugadorController.java
+++ src/main/java/com/uade/carreracaballos/controller/JugadorController.java
@@ -40,14 +40,6 @@ public class JugadorController {
 		this.jugadorSeleccionado = jugador;
 	}
 
-	/*public void seleccionarCaballo(Caballo caballo) {
-		// hay que hacer que el caballo se reciba por id, y que el caballo service devuelva el caballo partiendo de esa id.
-		// dantenota: no se como hacer, porque segun claude los controllers deberian hablar entre si, y segun gemini los service severian hablar entre si
-		if (jugadorSeleccionado == null)
-			throw new RuntimeException("Error: no se seleccionó un jugador");
-		jugadorSeleccionado.seleccionarCaballo(caballo);
-	}*/
-
 	public void seleccionarCaballo(int id) { // 
 		if (jugadorSeleccionado == null)
 			throw new RuntimeException("Error: no se seleccionó un jugador");
