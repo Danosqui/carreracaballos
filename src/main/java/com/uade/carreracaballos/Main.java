e6d3d78e985948d5a0145d924f97444e3c7ec9af
JuanValcarce <vjuan@frba.utn.edu.ar>
2026-06-08 21:29:01 -0300

    Update Main.java

--- src/main/java/com/uade/carreracaballos/Main.java
+++ src/main/java/com/uade/carreracaballos/Main.java
@@ -7,8 +7,5 @@ import com.uade.carreracaballos.view.MenuFrame;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new MenuFrame().setVisible(true));
-
-        // Para ejecutar la CLI en vez de la GUI, comentar la linea de arriba y descomentar la de abajo:
-        //CLI.main(args);
     }
 }
